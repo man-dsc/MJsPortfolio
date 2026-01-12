f599b1ff81630360d8cf72685127e9fb9de2ab0e
man-dsc <manjeet.dev1304@gmail.com>
2026-01-11 21:13:43 -0500

    fixed names of files

--- navbar.js
+++ navbar.js
@@ -2,22 +2,22 @@ document.getElementById('nav-placeholder').innerHTML = `
   <!-- Nav -->
 <nav id="nav">
     <ul>
-        <li><a href="/JaysAnalystPortfolio/index.html">Home</a></li>
-        <li><a href="/JaysAnalystPortfolio/About.html">About</a></li>
+        <li><a href="/Portfolio/index.html">Home</a></li>
+        <li><a href="/Portfolio/About.html">About</a></li>
         <li>
-            <a href="/JaysAnalystPortfolio/Projects.html">Projects</a>
+            <a href="/Portfolio/Projects.html">Projects</a>
             <ul>
-                <li><a href="/JaysAnalystPortfolio/Projects/books.html">Book Reviews</a></li>
-                <li><a href="/JaysAnalystPortfolio/scraper/DataHome.html">Investment Tools</a></li>
-                <li><a href="/JaysAnalystPortfolio/Projects/MacroResearch.html">Macro Research</a></li>
+                <li><a href="/Portfolio/Projects/books.html">Book Reviews</a></li>
+                <li><a href="/Portfolio/scraper/DataHome.html">Investment Tools</a></li>
+                <li><a href="/Portfolio/Projects/MacroResearch.html">Macro Research</a></li>
                 
                 
                 
-                <li><a href="/JaysAnalystPortfolio/Projects/EMR.html">EMR Management</a></li>
-                <li><a href="/JaysAnalystPortfolio/Projects/CarPrice.html">Car Price Predictor</a></li>
+                <li><a href="/Portfolio/Projects/EMR.html">EMR Management</a></li>
+                <li><a href="/Portfolio/Projects/CarPrice.html">Car Price Predictor</a></li>
             </ul>
         </li>
-        <li><a href="/JaysAnalystPortfolio/Resume.html">Resume</a></li>
+        <li><a href="/Portfolio/Resume.html">Resume</a></li>
         <li><a href="https://www.linkedin.com/in/mj--dev/">LinkedIn</a></li>
     </ul>
 </nav>
